0a70e168ce2db1fea3dbdfc9d83baa121aefa13d
Gustavo <falcionegustavo@gmail.com>
2022-03-13 18:35:36 -0300

    finalizando logica para determinar o vencedor

--- interface.js
+++ interface.js
@@ -11,18 +11,16 @@ function handleClick(event) {
   let square = event.target;
   let position = square.id;
 
-  handleMove(position);
-  updateSquares();
+  if (handleMove(position)) {
+    setTimeout(() => {
+      alert(`O jogo acabou - o vencedor foi o player ${playerTime}`);
+    }, 10);
+  }
+  updateSquare(position);
 }
 
-function updateSquares() {
-  let squares = document.querySelectorAll(".square");
-  squares.forEach((square) => {
-    let position = square.id;
-    let symbol = board[position];
-
-    if (symbol != "") {
-      square.innerHTML = `<div class="${symbol}"></div>`;
-    }
-  });
+function updateSquare(position) {
+  let square = document.getElementById(position.toString());
+  let symbol = board[position];
+  square.innerHTML = `<div class="${symbol}"></div>`;
 }
